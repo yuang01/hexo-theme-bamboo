642ba599f63dec8a527be4d9c445e72ff492f765
yuang01 <1730241541@qq.com>
2024-02-08 11:30:49 +0800

    update goTop js

--- source/js/goTop.js
+++ source/js/goTop.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
   const backBtn = document.querySelector('.goTop');
-  
+  isShow();
   // 判断是否显示返回顶部按钮
   window.addEventListener('scroll', function () {
+    isShow();
+  });
+  function isShow() {
     if (window.scrollY > 100) {
       backBtn.style.display = 'block';
       backBtn.style.transform = 'translateY(0)';
     } else {
       backBtn.style.transform = 'translateY(120px)';
     }
-  });
-  
+  }
   // 返回顶部按钮点击事件
   backBtn.addEventListener('click', function () {
     scrollToTop(500); // 滚动到顶部，时长为500ms
